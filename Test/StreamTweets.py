3319674f9b671771d5bdf9e1e72bba33de4a56a8
Kim HyunWoo <stories2@naver.com>
2018-03-01 21:13:12 +0900

    Tests : crawl tweets

--- Test/StreamTweets.py
+++ Test/StreamTweets.py
@@ -1,6 +1,7 @@
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+import json
 
 consumer_key        = ''
 consumer_secret     = ''
@@ -9,15 +10,33 @@ access_token_secret = ''
 
 class listener(StreamListener):
 
+    def __init__(self):
+        self.dataDic = []
+
     def on_data(self, data):
-        print("Data:", data)
+        print("# " + str(self.dataDic.__len__()) + " Data:", data)
+        objData = json.loads(data)
+        self.dataDic.append(objData)
         return True
 
     def on_error(self, status):
         print("Error:", status)
 
+    def get_data(self):
+        return self.dataDic
+
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-twitterStream = Stream(auth, listener())
-twitterStream.filter(track=["@TTestServer"])
+twitterListener = listener()
+
+try:
+    twitterStream = Stream(auth, twitterListener)
+    twitterStream.filter(track=["a"])
+except:
+
+    twitterStream.running = False
+    print("exporting data")
+    with open('data.json', 'w') as outfile:
+        json.dump(twitterListener.get_data(), outfile)
+    print("export data ok")
